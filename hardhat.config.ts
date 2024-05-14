a54b3042d5cc2170006ac20dfe43373e823cc51d
QuangNguyen <quangnguyenngoc314@gmail.com>
2024-05-15 00:48:20 +0700

    ok 1

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@nomicfoundation/hardhat-ignition-ethers";
+require('dotenv').config();
+
+const PRIVATE_KEY: any = process.env.PRIVATE_KEY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: `https://ethereum-sepolia-rpc.publicnode.com`,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
